682bb663b2465e8621a8e825493a6e856849223f
Ugis Berzins <berzinsu@gmail.com>
2018-02-17 16:07:51 +0200

    Add icons to player and make them white

--- src/cljs/itchallenge/player/views.cljs
+++ src/cljs/itchallenge/player/views.cljs
@@ -8,7 +8,7 @@
 
 (defn song-time []
   [:div {:style {:font-weight   400
-                 :font-size     42
+                 :font-size     48
                  :margin-bottom 20}}
    "00:34"])
 
@@ -22,8 +22,6 @@
                  :font-weight 100}}
    "Frédéric Chopin"])
 
-
-
 (defn song-info []
   [:div {:style {:color           :white
                  :flex-direction  :column
@@ -35,29 +33,56 @@
    [author]])
 
 
+(defn playlist-btn []
+  [:object {:type  "image/svg+xml"
+            :data  "icons/playlist.svg"
+            :class "icon"}])
+
+(defn play-pause-btn []
+  [:object {:type  "image/svg+xml"
+            :data  "icons/playpause.svg"
+            :class "icon"}])
+
+(defn next-btn []
+  [:object {:type  "image/svg+xml"
+            :data  "icons/next.svg"
+            :class "icon"}])
+
+(defn prev-btn []
+  [:object {:type  "image/svg+xml"
+            :data  "icons/prev.svg"
+            :class "icon"}])
+
 (defn inner-circle []
   [:div {:style {:border-radius   "50%"
                  :border          "1px solid white"
-                 :height          250
-                 :width           250
+                 :height          290
+                 :width           290
                  :display         :flex
                  :flex-direction  :column
                  :align-items     :center
-                 :justify-content :center}}
+                 :justify-content :center
+                 :margin          "15px 10px"}}
    [song-info]])
 
 (defn outer-circle []
   [:div {:style {:border-radius   "50%"
                  :border          "1px solid white"
-                 :height          350
-                 :width           350
+                 :height          400
+                 :width           400
                  :position        :absolute
                  :top             0
                  :left            0
                  :display         :flex
+                 :flex-direction  :column
                  :align-items     :center
                  :justify-content :center}}
-   [inner-circle]])
+   [playlist-btn]
+   [:div {:style {:display :flex}}
+    [prev-btn]
+    [inner-circle]
+    [next-btn]]
+   [play-pause-btn]])
 
 (defn player []
   [:div {:style {:position        :relative
